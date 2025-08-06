eae85a3b54f6493037882f294d530e81eff8f50d
aiclubiitropar <116072638+aiclubiitropar@users.noreply.github.com>
2025-08-07 00:11:35 +0530

    Add files via upload

---
+++ mirage-client/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import MirageUI from "./components/mirageui";
+
+function App() {
+  return <MirageUI />;
+}
+
+export default App;
